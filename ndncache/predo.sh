cfac1b1cf0078f657167f296315791d6eb84f476
Melon Li <lookat119@163.com>
2016-03-21 16:06:58 +0800

    add ndncache/broanalyze

--- ndncache/predo.sh
+++ ndncache/predo.sh
@@ -2,12 +2,12 @@
 ps axu | grep nova | grep color -v | grep grep -v | awk '{cmd="sudo kill -9 "$2;system(cmd)}'
 ps axu | grep neutron | grep color -v | grep grep -v | awk '{cmd="sudo kill -9 "$2;system(cmd)}'
 ps axu | grep ovs | grep color -v | grep grep -v | awk '{cmd="sudo kill -9 "$2;system(cmd)}'
-mkfs -t ext4 -q /dev/ram0 28g
+mkfs -t ext4 -q /dev/ram0 10g
 if [ ! -d "/home/node1/Documents/n2disk" ];then
     mkdir -p /home/node1/Documents/n2disk
 fi
 
-chmod -R 777 /home/node1/Documents/n2disk
 mount /dev/ram0 /home/node1/Documents/n2disk
 
+chmod -R 777 /home/node1/Documents/n2disk
 
