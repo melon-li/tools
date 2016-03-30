7c0ed44da3e2dddd2a75fc6ea0eee1a41c1c975a
Melon Li <lookat@163.com>
2016-03-30 09:58:59 +0800

    Modify broanalyze

--- ndncache/test.sh
+++ ndncache/test.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
-while [ 1 ]
+#!/bin/bash -e
+
+if echo $1 | egrep -q '^[0-9]+$';then 
+    echo "$1 is a number"
+else
+    echo "$1 is not a number"
+fi
+exit 0
+for i in `seq 100`
 do
-  v=1
+cksum_v=`echo -n $i | cksum | awk '{print $1}'`
+echo $(($chsum_v%2))
 done
